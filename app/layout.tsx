01c5ceb5243e030a8c06ca3f26fb1ad7f006fbeb
IgorLAP <ilap1036@gmail.com>
2025-02-24 09:21:01 -0300

    chore: add clerk auth
    Signed-off-by: IgorLAP <ilap1036@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import { Providers } from "@/config/providers/Providers";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,12 +26,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <Providers>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </Providers>
   );
 }
---
+++ config/providers/Providers.tsx
@@ -0,0 +1,9 @@
+import { ClerkProvider } from "@clerk/nextjs"
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider afterSignOutUrl="/">
+      {children}
+    </ClerkProvider>
+  )
+}
